6b4b6958e834b658e04e906965e6523c9e8f3032
ahsanayaz1800 <ahsanayaz1800@gmail.com>
2026-04-04 17:46:22 +0500

    latest

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,28 @@
+// import "slick-carousel/slick/slick.css";
+// import "slick-carousel/slick/slick-theme.css";
+// import "@/styles/globals.css";
+
+// export default function App({ Component, pageProps }) {
+//   return <Component {...pageProps} />;
+// }
+
+
+
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import "@/styles/globals.css";
+import Script from "next/script";
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Script
+        src="https://analytics.ahrefs.com/analytics.js"
+        data-key="Ghi1WGCge8Rt2PL4cschnA"
+        strategy="afterInteractive"
+      />
+      
+      <Component {...pageProps} />
+    </>
+  );
 }
