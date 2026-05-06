1f6dc50065e4c7dc7b00ebc24342abe58c8f6217
ahsanayaz1800 <ahsanayaz1800@gmail.com>
2026-05-06 23:30:17 +0500

    11:20 seo updated

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -8,9 +8,33 @@
 //   ],
 // };
 
+// module.exports = {
+//   siteUrl: "https://brandschamber.com",
+//   generateRobotsTxt: true,
+
+//   exclude: [
+//     "/checkout",
+//     "/payment/*",
+//   ],
+
+//   robotsTxtOptions: {
+//     policies: [
+//       {
+//         userAgent: "*",
+//         allow: "/",
+//       },
+//     ],
+//     additionalSitemaps: [
+//       "https://brandschamber.com/sitemap.xml",
+//     ],
+//   },
+// };
+
+
 module.exports = {
   siteUrl: "https://brandschamber.com",
   generateRobotsTxt: true,
+  trailingSlash: false,
 
   exclude: [
     "/checkout",
@@ -24,8 +48,16 @@ module.exports = {
         allow: "/",
       },
     ],
-    additionalSitemaps: [
-      "https://brandschamber.com/sitemap.xml",
-    ],
+  },
+
+  transform: async (config, path) => {
+    const cleanPath = path === "/" ? "/" : path.replace(/\/$/, "");
+
+    return {
+      loc: `${config.siteUrl}${cleanPath}`,
+      changefreq: "daily",
+      priority: 0.7,
+      lastmod: new Date().toISOString(),
+    };
   },
 };
